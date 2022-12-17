861b4e77024684a651103ca30ec85a7346d5bc8d
Egor Aleksandrovich <egor09662@gmail.com>
2022-12-17 18:59:59 +0100

    tab bar

---
+++ 1/FifthViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FifthViewController.swift
+//  1
+//
+//  Created by egor aleksandrovich on 17.12.2022.
+//
+
+import UIKit
+
+class FifthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
